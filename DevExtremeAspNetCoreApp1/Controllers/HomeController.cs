eab06d308b206ef8684a20ed3cba834a28f3ba74
Snehal <the_ss_2009@yahoo.in>
2021-10-06 20:20:25 +0530

    base create

---
+++ DevExtremeAspNetCoreApp1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DevExtremeAspNetCoreApp1.Models;
+
+namespace DevExtremeAspNetCoreApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new SampleTab { });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View();
+        }
+    }
+}
